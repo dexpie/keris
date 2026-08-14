57ac4fa36d3c0ca3bd5fb4afab86545c051cf773
dexpie <gadingputra103@gmail.com>
2026-08-14 15:38:50 +0700

    chore: bump to 0.3.2

--- keris/__init__.py
+++ keris/__init__.py
@@ -9,7 +9,7 @@ def _version() -> str:
     try:
         return metadata.version("keris-toolkit")
     except metadata.PackageNotFoundError:
-        return "0.3.1"
+        return "0.3.2"
 
 
 __version__ = _version()
